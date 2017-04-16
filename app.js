2d344ce98cefa32816de0f2b32063cc8bba258ae
David Kurutz <davidkurutz@gmail.com>
2017-04-15 23:39:17 -0400

    implemented stylus

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var pug = require('pug');
+var stylus = require('stylus');
+var nib = require('nib');
 
 var routes = require('./routes/all');
 
@@ -13,6 +15,13 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(stylus.middleware({
+  src: path.join(__dirname, "public/stylesheets"),
+  compile: function(str, p) {
+    return stylus(str).set("filename", p).use(nib());
+  }
+}))
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
